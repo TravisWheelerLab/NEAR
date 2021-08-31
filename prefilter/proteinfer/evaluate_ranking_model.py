18949805db2c9ec680eec5ddf14d834d40866af7
thomas colligan <thomas.colligan@umontana.edu>
2021-08-31 11:56:51 -0600

    Finish ranking all sequences by their family score and add some notes

--- prefilter/proteinfer/evaluate_ranking_model.py
+++ prefilter/proteinfer/evaluate_ranking_model.py
@@ -48,27 +48,8 @@ def load_model(logs_dir, model_path):
     model.eval()
     return model, hparams
 
-
-if __name__ == '__main__':
-    args = parser()
-    test_files = glob(os.path.join(args.test_data, "*test.json"))
-
-    model, hparams = load_model(args.logs_dir, model_path=args.model_path)
-
-    pfam_id_to_class_code = hparams['class_code_mapping']
-    class_code_to_pfam_id = {v: k for k, v in pfam_id_to_class_code.items()}
-
-    test_psd = ProteinSequenceDataset(test_files, pfam_id_to_class_code, evaluating=True)
-    decoys = SimpleSequenceEmbedder('/home/tc229954/data/prefilter/small-dataset/random_sequences/random_sequences.fa')
-
-    batch_size = 32
-    test_dataset = torch.utils.data.DataLoader(test_psd,
-                                               batch_size=batch_size,
-                                               shuffle=False)
-
-    decoy_dataset = torch.utils.data.DataLoader(decoys,
-                                                batch_size=batch_size,
-                                                shuffle=False)
+def predict_all_sequences_and_rank(test_dataset, decoy_dataset, save_fig,
+                                   batch_size=32):
 
     # get final classification layer's weights and init a new
     # layer with trained weights
@@ -108,21 +89,46 @@ if __name__ == '__main__':
             if list_of_scores.ndim < 2:
                 list_of_scores = np.expand_dims(list_of_scores, axis=0)
             decoys = np.concatenate(decoy_scores)
-            decoys_and_scores = np.concatenate((list_of_scores, decoys), axis=0)
-            print(decoys_and_scores.shape)
 
-            for threshold in range(10, 100, 2)[::-1]:
+            real_sequences_but_not_from_same_family = []
+            for cc, scores in family_id_to_score_and_label.items():
+                if cc != family_class_code:
+                    real_sequences_but_not_from_same_family.extend(scores)
+
+            real_sequences_but_not_from_same_family = np.stack(real_sequences_but_not_from_same_family)
+
+            print(decoys.shape, list_of_scores.shape, real_sequences_but_not_from_same_family.shape)
+
+            for threshold in range(10, 100, 2):
+
                 threshold = threshold / 100
-                thresholded_preds = list_of_scores.copy()
-                thresholded_preds[thresholded_preds >= threshold] = 1
-                thresholded_preds[thresholded_preds < threshold] = 0
-                num_true_positives_above_threshold = np.count_nonzero(thresholded_preds[:, family_class_code])
-                num_false_positives_above_threshold = np.count_nonzero(thresholded_preds) - num_true_positives_above_threshold
-                threshold_to_true_positives[threshold] += num_true_positives_above_threshold
-                threshold_to_false_positives[threshold] += num_false_positives_above_threshold
+                thresholded_true_scores = list_of_scores.copy()
+                thresholded_true_scores[thresholded_true_scores >= threshold] = 1
+                thresholded_true_scores[thresholded_true_scores < threshold] = 0
 
+                thresholded_decoys = decoys.copy()
+                thresholded_decoys[thresholded_decoys >= threshold] = 1
+                thresholded_decoys[thresholded_decoys < threshold] = 0
+
+                thresholded_real = real_sequences_but_not_from_same_family.copy()
+                thresholded_real[thresholded_real >= threshold] = 1
+                thresholded_real[thresholded_real < threshold] = 0
+
+                num_true_positives_above_threshold = np.count_nonzero(thresholded_true_scores[:, family_class_code])
+                num_false_positives_above_threshold_decoys = np.count_nonzero(thresholded_decoys[:, family_class_code])
+                num_false_positives_above_threshold_real_sequences = np.count_nonzero(thresholded_real[:, family_class_code])
+                threshold_to_true_positives[threshold] += num_true_positives_above_threshold
+                threshold_to_false_positives[threshold] += (num_false_positives_above_threshold_decoys)#+ num_false_positives_above_threshold_real_sequences)
+
+    # sequence ideally: shuffled sequence will be thrown out
+    # x-axis: cutoff sigmoid probability
+    # y-axis: what percent of the things that I hope will get run thru hmmer actually do (percent recovery)
+    # y-axis, 2nd plot: what percent of random shit gets puts through
+    # set up collection of families
+
+    # now I need to look at #number of families passed through at different sigmoid thresholds
     from pprint import pprint
-    total_sequences = len(test_dataset)
+    total_sequences = len(test_psd)
     percent_tps_recovered = {k: v/total_labels for k, v in threshold_to_true_positives.items()}
     mean_fps_per_sequence = {k: v/total_sequences for k, v in threshold_to_false_positives.items()}
     pprint(percent_tps_recovered)
@@ -134,5 +140,28 @@ if __name__ == '__main__':
     ax.set_ylabel('fraction of true positives')
     ax.set_xlabel('mean false positives per sequence')
     ax.set_title('0.5pid clusters, decoys included')
-    plt.savefig(args.save_fig)
+    plt.savefig(save_fig)
     plt.close()
+
+if __name__ == '__main__':
+    args = parser()
+    test_files = glob(os.path.join(args.test_data, "*test.json"))
+
+    model, hparams = load_model(args.logs_dir, model_path=args.model_path)
+
+    pfam_id_to_class_code = hparams['class_code_mapping']
+    class_code_to_pfam_id = {v: k for k, v in pfam_id_to_class_code.items()}
+
+    test_psd = ProteinSequenceDataset(test_files, pfam_id_to_class_code, evaluating=True)
+    decoys = SimpleSequenceEmbedder('/home/tc229954/data/prefilter/small-dataset/random_sequences/random_sequences.fa')
+    test_dataset = torch.utils.data.DataLoader(test_psd,
+                                               batch_size=batch_size,
+                                               shuffle=False)
+
+    decoy_dataset = torch.utils.data.DataLoader(decoys,
+                                                batch_size=batch_size,
+                                                shuffle=False)
+
+    batch_size = 32
+
+    predict_all_sequences_and_rank(test_psd, decoys, args.save_fig)
