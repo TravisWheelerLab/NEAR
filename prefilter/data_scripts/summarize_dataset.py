431de6c8f8288f3396936453a72430fdcbb4a8eb
thomas colligan <thomas.colligan@umontana.edu>
2021-09-03 09:23:07 -0600

    Add statistics on number of sequences by family

--- prefilter/data_scripts/summarize_dataset.py
+++ prefilter/data_scripts/summarize_dataset.py
@@ -13,9 +13,14 @@ def summarize_dataset(json_files, savefig=None):
     sequence_to_count = defaultdict(int)
     labels_per_sequence = []
 
+    family_to_count = defaultdict(int)
     for f in json_files:
         with open(f, 'r') as src:
             sequence_to_labels = json.load(src)
+        for sequence, label_list in sequence_to_labels.items():
+            for label in label_list:
+                family_to_count[label] += 1
+
         sequences = list(sequence_to_labels.keys())
         labels = list(sequence_to_labels.values())
         l = list(map(len, sequences))
@@ -32,8 +37,20 @@ def summarize_dataset(json_files, savefig=None):
                  np.percentile(lengths, 75),
                  len(lengths))
 
+    spf = 'mean members: {:.3f}, median: {:.3f}, 25th percentile: {:.3f}\n 75th'
+    spf += ' percentile {:.3f}, number of families. {:d}\n'
+    spf += ' min members: {:.3f}, max members: {:.3f} '
+    x = list(family_to_count.values())
+    spf = spf.format(np.mean(x),
+                     np.median(x),
+                     np.percentile(x, 25),
+                     np.percentile(x, 75),
+                     len(family_to_count),
+                     np.min(x),
+                     np.max(x))
+
     if savefig is not None:
-        fig, ax = plt.subplots(ncols=2, figsize=(13, 10))
+        fig, ax = plt.subplots(ncols=3, figsize=(13, 10))
 
         ax[0].set_title(s, fontsize=8)
         ax[0].hist(lengths, bins=100, histtype='step')
@@ -43,14 +60,19 @@ def summarize_dataset(json_files, savefig=None):
         ax[1].hist(labels_per_sequence, bins=100, histtype='step')
         ax[1].set_xlabel('num labels')
 
+        ax[2].set_title(spf, fontsize=8)
+        ax[2].hist(list(family_to_count.values()), bins=50, histtype='step')
+        ax[1].set_xlabel('members in family')
         plt.savefig(savefig)
         plt.close()
 
 
 def parser():
     ap = ArgumentParser()
-    ap.add_argument('--directory', required=True, type=str)
-    ap.add_argument('--glob_string', required=True, type=str)
+    ap.add_argument('--directory', required=True, type=str,
+                    help='where json files are stored')
+    ap.add_argument('--glob_string', required=True, type=str,
+                    help='which pattern you want to match')
     ap.add_argument('--save_fig', type=str)
     return ap.parse_args()
 
