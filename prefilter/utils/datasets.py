1ae922a42183c2f8e7ff218f138fb0471248681b
thomas colligan <thomas.colligan@umontana.edu>
2021-12-10 14:24:05 -0700

    Fix length of dataset to include 10 examples per family

--- prefilter/utils/datasets.py
+++ prefilter/utils/datasets.py
@@ -150,6 +150,8 @@ class ProteinSequenceDataset(torch.utils.data.Dataset):
                         family_to_resampled_membership[family] = kept
 
             self.length_of_dataset = sum(list(family_to_resampled_membership.values()))
+            if self.resample_based_on_uniform_dist:
+                self.length_of_dataset = 10*self.length_of_dataset
             self.families = np.asarray(list(family_to_resampled_membership.keys()))
             self.sample_probs = np.asarray(
                 list(family_to_resampled_membership.values())
