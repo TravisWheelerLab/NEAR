beb10e8828949cd0712760b88ad883fabdca9ac7
thomas colligan <thomas.colligan@umontana.edu>
2022-01-10 14:21:41 -0700

    Remove .sh files added by accident

--- prefilter/utils/hdb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-f=clustered/0.5/CGI-121.0.5-train.fa
-
-/home/tc229954/anaconda/envs/prefilter/bin/python \
-    /home/tc229954/share/prefilter/prefilter/utils/label_fasta.py hdb $f Pfam-A.seed
