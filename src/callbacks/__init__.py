6d006799346e275e1a6ebe96e9f5a9dec50f432b
thomas colligan <thomas.colligan@umontana.edu>
2022-09-08 14:13:47 -0600

    Add another callback that saves multiple well-performing model

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -12,6 +12,15 @@ class CallbackSet:
         auto_insert_metric_name=False,
         save_top_k=1,
     )
+    _callbacks.append(checkpoint_callback)
+
+    checkpoint_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
+        monitor="val_loss",
+        mode="min",
+        filename="epoch_{epoch}_{val_loss:.5f}",
+        auto_insert_metric_name=False,
+        save_top_k=10,
+    )
 
     _callbacks.append(checkpoint_callback)
 
