8cdd4894e418e5dfd46a470f6613975c649503ec
thomas colligan <thomas.colligan@umontana.edu>
2022-09-14 15:02:33 -0600

    Add small working vae. Now to incorporate the CNN

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
+import logging
+
 from sacred import Experiment
+
+logger = logging.getLogger("train")
+logger.setLevel(logging.WARNING)
 
 train_ex = Experiment()
 
@@ -6,35 +11,40 @@ train_ex = Experiment()
 @train_ex.config
 def config():
 
-    description = "A test of the contrastive dot product model (with normalization)."
+    description = "A contrastive model for testing."
     gpus = 1
     num_nodes = 1
-    num_workers = 12
+    num_workers = 0
     check_val_every_n_epoch = 1
     log_dir = "model_data/sept6/"
     batch_size = 32
-    epochs = 100
+    epochs = 30
     learning_rate = 1e-4
-    seq_len = 150
-    log_interval = 1000
+    seq_len = 32
+    log_interval = 100
 
-    model_name = "ResNet1d"
-    dataset_name = "SwissProtGenerator"
+    model_name = "SequenceVAE"
+    dataset_name = "SwissProtGeneratorDanielSequenceEncode"
 
     embed_msas = False
+    root = "/xdisk/twheeler/colligan/"
+
+    cnn_model_state_dict = f"{root}/data/prefilter/model_16.sdic"
 
     model_args = {
         "learning_rate": learning_rate,
         "log_interval": log_interval,
+        "cnn_model_state_dict": cnn_model_state_dict,
+        "initial_seq_len": seq_len,
     }
 
     train_dataset_args = {
-        "fa_file": "/home/u4/colligan/data/prefilter/uniprot_sprot.fasta",
+        "fa_file": f"{root}/data/prefilter/uniprot_sprot.fasta",
         "minlen": seq_len,
     }
 
     val_dataset_args = {
-        "fa_file": "/home/u4/colligan/data/prefilter/uniprot_sprot.fasta",
+        "fa_file": f"{root}/data/prefilter/uniprot_sprot.fasta",
         "minlen": seq_len,
         "training": False,
     }
--- src/models/sequence_vae.py
+++ src/models/sequence_vae.py
@@ -20,8 +20,7 @@ class SequenceVAE(pl.LightningModule):
         learning_rate,
         log_interval,
         cnn_model_state_dict,
-        cnn_model_args,
-        pool_factors=[2, 2, 2, 2],
+        initial_seq_len,
         training=True,
     ):
 
@@ -29,7 +28,7 @@ class SequenceVAE(pl.LightningModule):
 
         self.learning_rate = learning_rate
         self.training = training
-        self.pool_factors = pool_factors
+        self.initial_seq_len = initial_seq_len
 
         self.cnn_model_state_dict = cnn_model_state_dict
 
@@ -56,7 +55,7 @@ class SequenceVAE(pl.LightningModule):
             param.requires_grad = False
 
         self.res_block_kernel_size = 3
-        self.res_block_n_filters = 128
+        self.res_block_n_filters = 256
         # small number of residual blocks
         self.n_res_blocks = 12
         self.res_bottleneck_factor = 1
@@ -65,14 +64,13 @@ class SequenceVAE(pl.LightningModule):
         self.log_interval = log_interval
         self.loss_func = SupConLoss()
         self.one_hot_dimension = 20
+        self.layer_list = []
 
         self._setup_layers()
+        self.to("cuda")
         self.save_hyperparameters()
-        self.N = torch.distributions.Normal(0, 1)
-        if torch.cuda.is_available():
-            self.N.loc = self.N.loc.cuda()  # hack to get sampling on the GPU
-            self.N.scale = self.N.scale.cuda()
-        self.kl = 0
+        self.KLD = 0
+        self.xent = torch.nn.CrossEntropyLoss()
 
     def _setup_layers(self):
 
@@ -82,42 +80,43 @@ class SequenceVAE(pl.LightningModule):
             kernel_size=1,
         )
 
-        layer_list = [self.embed]
+        self.layer_list.append(self.embed)
+        z = ResConv(
+            self.res_block_n_filters,
+            kernel_size=self.res_block_kernel_size,
+            padding=self.padding,
+            padding_mode=self.padding_mode,
+        )
 
-        for i in range(self.n_res_blocks):
-
-            if (i + 1) % 3 == 0:
-                layer_list.append(torch.nn.AvgPool1d(self.pool_factors[i // 3]))
-
-            layer_list.append(
-                ResConv(
-                    self.res_block_n_filters,
-                    kernel_size=self.res_block_kernel_size,
-                    padding=self.padding,
-                    padding_mode=self.padding_mode,
-                )
+        self.layer_list.append(z)
+        self.layer_list.append(torch.nn.AvgPool1d(kernel_size=2))
+        self.layer_list.append(
+            ResConv(
+                self.res_block_n_filters,
+                kernel_size=self.res_block_kernel_size,
+                padding=self.padding,
+                padding_mode=self.padding_mode,
             )
-
-        self.embedding_trunk = torch.nn.Sequential(*layer_list)
-
+        )
         sigma_mlp_list = [
-            torch.nn.Linear(128 * 8, 4096),
-            torch.nn.ELU(),
-            torch.nn.Linear(4096, 4096),
+            torch.nn.Linear(
+                self.res_block_n_filters * self.initial_seq_len // 2,
+                self.res_block_n_filters * self.initial_seq_len // 2,
+            ),
         ]
         mu_mlp_list = [
-            torch.nn.Linear(128 * 8, 4096),
-            torch.nn.ELU(),
-            torch.nn.Linear(4096, 4096),
+            torch.nn.Linear(
+                self.res_block_n_filters * self.initial_seq_len // 2,
+                self.res_block_n_filters * self.initial_seq_len // 2,
+            ),
         ]
-
-        self.projector = torch.nn.Linear(4096, 128 * 8)
 
         self.sigma_mlp = torch.nn.Sequential(*sigma_mlp_list)
         self.mu_mlp = torch.nn.Sequential(*mu_mlp_list)
 
         upsample = torch.nn.Upsample(scale_factor=2)
 
+        # more complexity on the upsampling head.
         upsample_list = [
             ResConv(
                 self.res_block_n_filters,
@@ -132,21 +131,12 @@ class SequenceVAE(pl.LightningModule):
                 padding=self.padding,
                 padding_mode=self.padding_mode,
             ),
-            upsample,
             ResConv(
                 self.res_block_n_filters,
                 kernel_size=self.res_block_kernel_size,
                 padding=self.padding,
                 padding_mode=self.padding_mode,
             ),
-            upsample,
-            ResConv(
-                self.res_block_n_filters,
-                kernel_size=self.res_block_kernel_size,
-                padding=self.padding,
-                padding_mode=self.padding_mode,
-            ),
-            upsample,
             torch.nn.Conv1d(
                 in_channels=self.res_block_n_filters,
                 out_channels=self.one_hot_dimension,
@@ -155,62 +145,60 @@ class SequenceVAE(pl.LightningModule):
                 padding_mode=self.padding_mode,
             ),
         ]
-
         self.upsampler = torch.nn.Sequential(*upsample_list)
+        self.final_conv = torch.nn.Conv1d(
+            in_channels=self.one_hot_dimension,
+            out_channels=self.one_hot_dimension,
+            kernel_size=1,
+            padding=self.padding,
+            padding_mode=self.padding_mode,
+        )
 
     def forward(self, x):
-        x = self.embedding_trunk(x)
-        sigma2 = self.sigma_mlp(x.reshape(-1, x.shape[-1] * x.shape[-2]))
+        for layer in self.layer_list:
+            layer = layer.to("cuda")
+            x = layer(x)
+        # flatten the embeddings,
+        # pass them into an MLP.
+        sigma = self.sigma_mlp(x.reshape(-1, x.shape[-1] * x.shape[-2]))
         mu = self.mu_mlp(x.reshape(-1, x.shape[-1] * x.shape[-2]))
-        sample = self.reparameterize(sigma2, mu)
-        logger.debug("fixme.")
-        sample = self.projector(sample)
-        # reconstructed = self.upsampler(sample.reshape(-1, x.shape[1], x.shape[2]))
-        return sample
+        sample = self.sample(sigma, mu).reshape(-1, x.shape[1], x.shape[2])
+        reconstruct = self.final_conv(self.upsampler(sample))
+        return sample, reconstruct
 
-    def reparameterize(self, sigma2, mu):
-        sigma2 = torch.exp(sigma2)
-        z = mu + sigma2 * self.N.sample(mu.shape)
-        self.kl = (sigma2**2 + mu**2 - torch.log(sigma2) - 1 / 2).sum()
-        return z
+    def sample(self, sigma, mu):
+        sigma = torch.exp(0.5 * sigma)
+        eps = torch.randn_like(sigma)
+        self.KLD = -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
+        return (eps * sigma) + mu
 
     def _shared_step(self, batch):
         features, masks, labelvecs = batch
-
-        (
-            sampled,
-            reconstructed,
-        ) = self.forward(features)
-
-        encoded_features = self.cnn_model(features)
-        encoded_reconstructed = self.cnn_model(reconstructed)
-
-        e1 = encoded_features.transpose(-1, -2)
-        e2 = encoded_reconstructed.transpose(-1, -2)
-
-        e1 = torch.cat(torch.unbind(e1, dim=0))
-        e2 = torch.cat(torch.unbind(e2, dim=0))
-        e1 = torch.nn.functional.normalize(e1, dim=-1)
-        e2 = torch.nn.functional.normalize(e2, dim=-1)
+        sampled, recon = self.forward(features)
+        # reconstruction loss
+        loss = self.xent(recon, features)
+        loss += self.KLD
 
         if self.global_step % self.log_interval == 0:
-            # what _should_ the output look like?
-            # let's think. What should
-            #
+
+            e1 = torch.cat(torch.unbind(recon, dim=0))
+            e2 = torch.cat(torch.unbind(features, dim=0))
             with torch.no_grad():
-                fig = plt.figure(figsize=(10, 10))
-                plt.imshow(
-                    torch.matmul(e1, e2.T).to("cpu").detach().numpy().astype(float),
+                fig, ax = plt.subplots(ncols=2)
+                ax[0].imshow(
+                    torch.nn.functional.softmax(e1, dim=1)
+                    .to("cpu")
+                    .numpy()
+                    .astype(float),
                     interpolation="nearest",
-                    vmin=-1,
-                    vmax=1,
                 )
-                plt.colorbar()
+                ax[1].imshow(
+                    e2.to("cpu").numpy().astype(float), interpolation="nearest"
+                )
                 self.logger.experiment.add_figure(
                     f"image", plt.gcf(), global_step=self.global_step
                 )
 
-        loss = self.loss_func(torch.cat((e1.unsqueeze(1), e2.unsqueeze(1)), dim=1))
         return loss
 
     def training_step(self, batch, batch_nb):
