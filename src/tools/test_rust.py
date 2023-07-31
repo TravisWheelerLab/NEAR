eed9938eb4dc07197019d74ee475c2ee89236b7d
Daphne Demekas <daphnedemekas@gmail.com>
2023-07-31 10:26:59 -0700

    test rust

--- src/tools/test_rust.py
+++ src/tools/test_rust.py
@@ -3,11 +3,16 @@ import numpy as np
 from random import choice
 from string import ascii_lowercase, digits
 
-scores_array = np.random.random(size=(5, 100, 1000))
-indices_array = np.random.randint(0, 100, size=(5, 100, 1000))
+
+scores_array = np.random.random(size=(100, 1000))
+indices_array = np.random.randint(0, 100, size=(100, 1000))
+
+
+# scores_array = np.random.random(size=(5, 100, 1000))
+# indices_array = np.random.randint(0, 100, size=(5, 100, 1000))
 
 chars = ascii_lowercase + digits
 unrolled_names = ["".join(choice(chars) for _ in range(2)) for _ in range(100)]
 
-scores_list = my_rust_module.filter_scores(scores_array,indices_array, unrolled_names)
+scores_list = my_rust_module.filter_scores(scores_array, indices_array, unrolled_names)
 print(scores_list)
