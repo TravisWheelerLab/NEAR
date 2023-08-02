bf6c08b336273626fcae208f3d1c3a58556a950e
Daphne Demekas <daphnedemekas@gmail.com>
2023-08-02 12:06:11 -0700

    make unrolled names a numpy array

--- src/tools/test_rust.py
+++ src/tools/test_rust.py
@@ -55,7 +55,9 @@ indices_array = np.random.randint(0, 100, size=(100, 1000))
 # indices_array = np.random.randint(0, 100, size=(5, 100, 1000))
 
 chars = ascii_lowercase + digits
-unrolled_names = ["".join(choice(chars) for _ in range(2)) for _ in range(100)]
+unrolled_names = np.array(
+    ["".join(choice(chars) for _ in range(2)) for _ in range(100)]
+)
 
 scores_list = my_rust_module.filter_scores(
     [scores_array], [indices_array], unrolled_names
