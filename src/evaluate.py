4121b27bcb179c826da7df1a4d199a974fc0c654
Daphne Demekas <daphnedemekas@gmail.com>
2023-08-05 16:58:03 -0700

    500

--- src/evaluate.py
+++ src/evaluate.py
@@ -144,12 +144,12 @@ def evaluate_for_times_mp(_config):
     queryfasta = FastaFile(params.query_file)
     query_sequences = queryfasta.data
 
-    # query_sequences = {
-    #    k: v
-    #    for k, v in zip(
-    #        list(query_sequences.keys())[:500], list(query_sequences.values())[:500]
-    #    )
-    # }
+    query_sequences = {
+        k: v
+        for k, v in zip(
+            list(query_sequences.keys())[:500], list(query_sequences.values())[:500]
+        )
+    }
 
     numqueries = len(query_sequences)
     print(f"Number of queries: {numqueries}")
