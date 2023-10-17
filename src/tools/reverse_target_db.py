d0da085afe8534a5a751969bf4d170dec8ec898a
Daphne Demekas <daphnedemekas@gmail.com>
2023-10-17 12:44:09 -0700

    filter by eval db

--- src/tools/reverse_target_db.py
+++ src/tools/reverse_target_db.py
@@ -3,19 +3,22 @@ from src.data.hmmerhits import FastaFile
 import tqdm
 
 _, hmmer_hits = load_hmmer_hits(4)
-
-
-targets_that_are_hits = set()
-
-for targetlist in hmmer_hits.values():
-    targetnames = list(targetlist.keys())
-    targets_that_are_hits.update(targetnames)
-
 targetfasta = FastaFile(
     f"/xdisk/twheeler/daphnedemekas/prefilter/data/targets-filtered.fa"
 )
 
 targetsequences = targetfasta.data
+
+targets_that_are_hits = set()
+
+for targetlist in tqdm.tqdm(hmmer_hits.values()):
+    targetnames = list(targetlist.keys())
+    for target in targetnames:
+        if target in targetsequences:
+            targets_that_are_hits.add(target)
+
+print(f"Number of targets that are hits: {len(targetnames)}")
+
 
 print(f"Number of target sequences: {len(targetsequences)}")
 
