c24427cf10f7e6acbc2c3d11a45b5237b101738a
Daphne Demekas <daphnedemekas@gmail.com>
2023-10-17 12:17:05 -0700

    fix paths

--- src/tools/reverse_target_db.py
+++ src/tools/reverse_target_db.py
@@ -1,6 +1,6 @@
 from src.data.compare_models import load_hmmer_hits
 from src.data.hmmerhits import FastaFile
-
+import tqdm
 
 _, hmmer_hits = load_hmmer_hits(4)
 
@@ -11,13 +11,19 @@ for targetlist in hmmer_hits.values():
     targetnames = list(targetlist.keys())
     targets_that_are_hits.update(targetnames)
 
-targetfasta = FastaFile(f"data/targets-filtered.fa")
+targetfasta = FastaFile(
+    f"/xdisk/twheeler/daphnedemekas/prefilter/data/targets-filtered.fa"
+)
 
 targetsequences = targetfasta.data
 
+print(f"Number of target sequences: {len(targetsequences)}")
+
 targets_to_reverse = []
 
-with open("/xdisk/twheeler/daphnedemekas/data/reversedtargets-filtered.fa", "w") as f:
-    for name, sequence in targetsequences.items():
+with open(
+    "/xdisk/twheeler/daphnedemekas/prefilter/data/reversedtargets-filtered.fa", "w"
+) as f:
+    for name, sequence in tqdm.tqdm(targetsequences.items()):
         if name not in targets_that_are_hits:
             f.write(f">{name}\n{sequence}\n")
