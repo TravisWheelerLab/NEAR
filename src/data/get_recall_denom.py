07fb6dd0ef903363a848e347415fee9a05e04cda
Daphne Demekas <daphnedemekas@gmail.com>
2023-10-20 10:26:30 -0700

    get num decoys

--- src/data/get_recall_denom.py
+++ src/data/get_recall_denom.py
@@ -19,22 +19,73 @@ def load_hmmer_hits(query_id: int = 4):
         raise Exception(f"No evaluation data for given query id {query_id}")
 
 
-all_hits_max, all_hits_normal = load_hmmer_hits(4)
+all_hits_max, _ = load_hmmer_hits(4)
 evalue_thresholds = [1e-10, 1e-4, 1e-1, 10]
 
 evalseqs = "/xdisk/twheeler/daphnedemekas/targetdataseqs/eval.txt"
 
 query_sequences = [
     t[:-4]
-    for t in os.listdir(
-        "/xdisk/twheeler/daphnedemekas/prefilter-output/AlignmentEvaluation/CPU-20K-50"
-    )
+    for t in os.listdir("/xdisk/twheeler/daphnedemekas/prefilter-output/CPU-5K-20")
 ]
 
 
-with open("target_names.txt", "r") as f:
-    target_sequences = [t.strip("\n") for t in f.readlines()]
-print(f"Found {len(query_sequences)} queries and {len(target_sequences)} targets")
+# with open("target_names.txt", "r") as f:
+#     target_sequences = [t.strip("\n") for t in f.readlines()]
+# print(f"Found {len(query_sequences)} queries and {len(target_sequences)} targets")
+
+
+def get_num_decoys(hmmer_hits):
+    with open("reversed-target-names.txt", "r") as f:
+        target_sequences = [t.strip("\n") for t in f.readlines()]
+    print(f"Found {len(query_sequences)} queries and {len(target_sequences)} targets")
+
+    num_pos_per_evalue = [0, 0, 0, 0]
+    num_marg_per_evalue = [0, 0, 0, 0]
+    num_decoys = [0, 0, 0, 0]
+
+    numhits = len(query_sequences) * len(target_sequences)
+    for query in query_sequences:
+        if query not in hmmer_hits:
+            continue
+
+        query_data = hmmer_hits[query]
+        for target in target_sequences:
+            if target not in query_data:
+                continue
+            data = query_data[target]
+            evalue = data[0]
+            if evalue < evalue_thresholds[3]:  # evalue < 10
+                num_pos_per_evalue[3] += 1
+                if evalue < evalue_thresholds[2]:  # evalue < e-1
+                    num_pos_per_evalue[2] += 1
+
+                    if evalue < evalue_thresholds[1]:
+                        num_pos_per_evalue[1] += 1
+
+                        if evalue < evalue_thresholds[0]:
+                            num_pos_per_evalue[0] += 1
+                        else:
+                            num_marg_per_evalue[0] += 1
+                    else:
+                        num_marg_per_evalue[0] += 1
+                        num_marg_per_evalue[1] += 1
+
+                else:  # e-1 < evalue < 10
+                    num_marg_per_evalue[0] += 1
+                    num_marg_per_evalue[1] += 1
+                    num_marg_per_evalue[2] += 1
+            else:
+                num_marg_per_evalue[0] += 1
+                num_marg_per_evalue[1] += 1
+                num_marg_per_evalue[2] += 1
+                num_marg_per_evalue[3] += 1
+            # numhits += 1
+    print(num_pos_per_evalue)
+    print(num_marg_per_evalue)
+    for i in range(4):
+        num_decoys[i] = numhits - num_pos_per_evalue[i] - num_marg_per_evalue[i]
+    return num_pos_per_evalue, num_decoys
 
 
 def get_numpos_per_evalue(hmmer_hits):
@@ -86,18 +137,28 @@ def get_numpos_per_evalue(hmmer_hits):
     return num_pos_per_evalue, num_decoys
 
 
-print("Getting recall denominator HMMER Max")
-num_pos_per_evaluemax, decoysmax = get_numpos_per_evalue(all_hits_max)
-print("Getting recall denominator HMMER Normal")
-num_pos_per_evaluenormal, decoysnormal = get_numpos_per_evalue(all_hits_normal)
+# print("Getting recall denominator HMMER Max")
+# num_pos_per_evaluemax, decoysmax = get_numpos_per_evalue(all_hits_max)
+# print("Getting recall denominator HMMER Normal")
+# num_pos_per_evaluenormal, decoysnormal = get_numpos_per_evalue(all_hits_normal)
 
-with open("hmmerrecall.txt", "w") as f:
+# with open("hmmerrecall.txt", "w") as f:
+#     f.write(f"HMMER MAX pos: {num_pos_per_evaluemax}" + "\n")
+#     f.write(f"HMMER MAX decoys: {decoysmax}")
+
+#     f.write("\n")
+#     f.write(f"HMMER Normal pos: {num_pos_per_evaluenormal}" + "\n")
+#     f.write(f"HMMER Normal decoys: {decoysnormal}")
+
+# f.close()
+
+
+print("Getting num decoys HMMER Max")
+num_pos_per_evaluemax, decoysmax = get_num_decoys(all_hits_max)
+
+with open("decoys.txt", "w") as f:
     f.write(f"HMMER MAX pos: {num_pos_per_evaluemax}" + "\n")
     f.write(f"HMMER MAX decoys: {decoysmax}")
-
-    f.write("\n")
-    f.write(f"HMMER Normal pos: {num_pos_per_evaluenormal}" + "\n")
-    f.write(f"HMMER Normal decoys: {decoysnormal}")
 
 f.close()
 
@@ -110,10 +171,10 @@ def prune(results):
             os.remove(f"{results}/{query}")
 
 
-# mmseqs = '/xdisk/twheeler/daphnedemekas/prefilter-output/mmseqs'
-# knn = '/xdisk/twheeler/daphnedemekas/prefilter-output/knn-for-homology'
-esm = "/xdisk/twheeler/daphnedemekas/prefilter-output/esm"
+# # mmseqs = '/xdisk/twheeler/daphnedemekas/prefilter-output/mmseqs'
+# # knn = '/xdisk/twheeler/daphnedemekas/prefilter-output/knn-for-homology'
+# esm = "/xdisk/twheeler/daphnedemekas/prefilter-output/esm"
 
-# prune(mmseqs)
-# prune(knn)
-# prune(esm)
+# # prune(mmseqs)
+# # prune(knn)
+# # prune(esm)
