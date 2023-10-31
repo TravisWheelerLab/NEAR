19ee98dc38d89279519d7a14afa8bd179a412ae6
Daphne Demekas <daphnedemekas@gmail.com>
2023-10-31 09:58:26 -0700

    udpate hmmer hits filepath

--- src/data/eval_data_config.py
+++ src/data/eval_data_config.py
@@ -1,4 +1,4 @@
-all_hits_max_file_4 = "data/evaltargetdictmax"
+all_hits_max_file_4 = "data/hmmerhits-masked"
 all_hits_normal_file_4 = "data/evaltargetdictnormal"
 # all_hits_max_file_4 = "data/evaluationtargetdict"
 # all_hits_normal_file_4 = "data/evaluationtargetdictnormal"
