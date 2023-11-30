0b839b538fd723bd5bbbc2de28e0a20fdcf64e83
Daphne Demekas <daphnedemekas@gmail.com>
2023-11-30 12:03:57 -0700

    update load inputs

--- src/data/eval_data_config.py
+++ src/data/eval_data_config.py
@@ -9,21 +9,26 @@ all_hits_max_file_4 = "data/hmmerhits-masked-dict"
 
 
 def load_inputs(hits, modelname, norm_q=True, norm_t=True):
-    if norm_q and not norm_t:
-        roc_filepath = f"ResNet1d/results/{modelname}_roc_norm_q.png"
-        temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_norm_q"
-
-    elif norm_t and not norm_q:
-        roc_filepath = f"ResNet1d/results/{modelname}_roc_norm_t.png"
-        temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_norm_t"
-
-    elif norm_t and norm_q:
-        roc_filepath = f"ResNet1d/results/{modelname}_roc_normalised.png"
-        temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_normalised"
-
-    else:
+    if "msv" in modelname:
         roc_filepath = f"ResNet1d/results/{modelname}_roc.png"
         temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}"
+    else:
+        if norm_q and not norm_t:
+            roc_filepath = f"ResNet1d/results/{modelname}_roc_norm_q.png"
+            temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_norm_q"
+        elif norm_t and not norm_q:
+            roc_filepath = f"ResNet1d/results/{modelname}_roc_norm_t.png"
+            temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_norm_t"
+
+        elif norm_t and norm_q:
+            roc_filepath = f"ResNet1d/results/{modelname}_roc_normalised.png"
+            temp_file = (
+                f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}_normalised"
+            )
+
+        else:
+            roc_filepath = f"ResNet1d/results/{modelname}_roc.png"
+            temp_file = f"/xdisk/twheeler/daphnedemekas/temp_files/{modelname}"
     if "masked" in all_hits_max_file_4:
         roc_filepath = roc_filepath[:-4] + "-masked.png"
         temp_file = temp_file + "-masked"
