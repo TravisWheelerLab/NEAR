1f6a2694da9aab06c1848722ad91164ecce9c3ad
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 <daphnedemekas@r5u20n1.puma.hpc.arizona.edu>
2024-01-17 10:11:01 -0700

    version from cluster

--- src/tools/time_index_construction.py
+++ src/tools/time_index_construction.py
@@ -2,17 +2,22 @@ import torch
 import faiss
 from src.utils.faiss_utils import create_faiss_index
 import time
+import numpy as np
 
 embedding_file = "/xdisk/twheeler/daphnedemekas/prefilter/data/targets_embeddings.pt"
+embeddings_file = "/xdisk/twheeler/daphnedemekas/NEAR-benchmarking/protbert/alltargets-masked.npy"
+#embeddings = torch.load(embedding_file)
+unrolled_targets = np.load(embeddings_file, allow_pickle=True)
+unrolled_targets = np.concatenate(unrolled_targets)
+print(unrolled_targets.shape)
 
-embeddings = torch.load(embedding_file)
-unrolled_targets = torch.cat(embeddings, dim=0)
+#unrolled_targets = torch.cat(embeddings, dim=0)
 
 index_device = "cuda"
 
 start = time.time()
 
-faiss.Index = create_faiss_index(
+index = create_faiss_index(
     embeddings=unrolled_targets,
     embed_dim=unrolled_targets.shape[-1],
     distance_metric="cosine",
@@ -23,3 +28,5 @@ faiss.Index = create_faiss_index(
 duration = time.time() - start
 
 print(f"Index Creation took: {duration}.")
+
+faiss.write_index(index, "/xdisk/twheeler/daphnedemekas/NEAR-benchmarking/protbert/index-gpu.index")
