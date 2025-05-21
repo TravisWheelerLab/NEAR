4b7739821a2987de5f3e49d8e26e946e18320b74
Daniel Olson <daniel.olson@me.com>
2025-05-20 20:33:32 -0600

    Filling out the arguments for each of the commands

--- src/near/main.py
+++ src/near/main.py
@@ -8,16 +8,78 @@ from .models import NEARResNet
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="NEAR: Neural Efficient Accurate Retrieval for sequence similarity search"
+        description="NEAR: Neural Embeddings for Amino acid Relationships"
+                    "A tool to quickly find similar sequence pairs in large datasets"
     )
 
     subparsers = parser.add_subparsers(dest='command', required=True)
 
     search_parser = subparsers.add_parser('search', help='Search using embeddings')
-    embed_parser = subparsers.add_parser('embed', help='Generate embeddings for sequences')
-    index_parser = subparsers.add_parser('index', help='Build search index')
-    train_parser = subparsers.add_parser('train', help='Train a NEAR model')
+    search_parser.add_argument('-o', '--output', type=str, default=None, required=True, help='Output file path')
 
+    search_parser.add_argument('-q', '--query', type=str, default=None,
+                               help='Path to the query fasta file')
+    search_parser.add_argument('--query_emb', type=str, default=None,
+                               help='Path to the query embedding file')
+
+    search_parser.add_argument('-t', '--target', type=str, default=None,
+                               help='Path to the target fasta file')
+    search_parser.add_argument('--target_index', type=str, default=None,
+                               help='Path to the target index file')
+    search_parser.add_argument('--target_emb', type=str, default=None,
+                               help='Path to the target embedding file')
+
+    search_parser.add_argument("--index_type", choices=["Default", "GPUCagra", "GPUCagraNN"],
+        default="Default",
+        help="Which backend to use (Default=GPUCagraNN, Cagra, or GPUCagraNN)")
+    search_parser.add_argument("--index_degree", type=int,
+                               default=64,
+                               help="The degree of graph used by the index")
+    search_parser.add_argument("--save_path", type=str,
+                               default=None,
+                               help="Where to save the index")
+    search_parser.add_argument("--discard_freq", type=int,
+                               default=16,
+                               help="The discard frequency for the index")
+    search_parser.add_argument("-k", "--top_k", type=int, default=64,
+                               help='Number of nearest neighbors to return for each query embedding')
+    search_parser.add_argument('--model_path', type=str, default=None,
+                               help='Path to the model json file. '
+                                    'Searches for local installation if unspecified.'
+                                    'Searches online if no local installation is found')
+    search_parser.add_argument('-d', '--device', type=str, default="cuda", help='Device to use for embedding')
+
+
+
+    embed_parser = subparsers.add_parser('embed', help='Generate embeddings for sequences')
+    embed_parser.add_argument('-o', '--output', type=str, default=None, required=True, help='Output file path')
+    embed_parser.add_argument('-i', '--input', type=str, default=None, required=True, help='Path to the input fasta file')
+    embed_parser.add_argument('-m', '--model', type=str, default=None, help='Path to the model json file')
+    embed_parser.add_argument('-d', '--device', type=str, default="cuda", help='Device to use for embedding')
+
+    index_parser = subparsers.add_parser('index', help='Build search index')
+    index_parser.add_argument('-o', '--output', type=str, default=None, required=True, help='Output file path')
+    index_parser.add_argument('-i', '--input', type=str, default=None, required=True, help='Path to the input fasta file')
+    index_parser.add_argument('-m', '--model', type=str, default=None, help='Path to the model json file')
+    index_parser.add_argument('-d', '--device', type=str, default="cuda", help='Device to use for embedding')
+    index_parser.add_argument("--index_type", choices=["Default", "GPUCagra", "GPUCagraNN"],
+        default="Default",
+        help="Which backend to use (Default=GPUCagraNN, Cagra, or GPUCagraNN)")
+    index_parser.add_argument("--index_degree", type=int,
+                               default=64,
+                               help="The degree of graph used by the index")
+    index_parser.add_argument("--save_path", type=str,
+                               default=None,
+                               help="Where to save the index")
+    index_parser.add_argument("--discard_freq", type=int,
+                               default=16,
+                               help="The discard frequency for the index")
+
+    train_parser = subparsers.add_parser('train', help='Train a NEAR model')
+    index_parser.add_argument('-i', '--input', type=str, default=None, required=True,
+                              help='training json file')
+    index_parser.add_argument('--print_example_json', action='store_true', default=False,
+                              help='Print an example json training file and exit')
     return parser.parse_args()
 
 
